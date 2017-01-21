2bd8bbfa01a8433fe4d4c35a92bf24c25fec93b6
Eric Ahnell <eric.ahnell@puttysoftware.com>
2017-01-21 12:23:14 -0500

    Add a water object as well.

---
+++ LaserTankGGJ2017/src/net/worldwizard/lasertank/objects/Water.java
@@ -0,0 +1,19 @@
+package net.worldwizard.lasertank.objects;
+
+import java.util.ArrayList;
+
+import net.worldwizard.lasertank.assets.GameImage;
+import net.worldwizard.lasertank.loaders.ImageLoader;
+
+public class Water extends GameObject {
+    public Water() {
+	super();
+	this.setName("Water");
+	this.setFrames(3);
+	ArrayList<GameImage> frames = new ArrayList<>();
+	frames.add(ImageLoader.loadObjectImage("water_1"));
+	frames.add(ImageLoader.loadObjectImage("water_2"));
+	frames.add(ImageLoader.loadObjectImage("water_3"));
+	this.setFrameAppearances(frames);
+    }
+}
